793427c82122a034e5b6fd95e05df5d15f53f389
Prakash-Balu <prakash.engg89@gmail.com>
2024-05-04 16:41:25 +0530

    updated changes
    updated changes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,4 +1,5 @@
 module.exports = { 'url' : 'mongodb+srv://prakashtech:Ahans%402021@cluster0.4rwwhvl.mongodb.net/veeras',
     'url1' : 'mongodb://127.0.0.1:/Edu',
+    'url2': 'mongodb+srv://vercel-admin-user:Ahans%402021@cluster0.4rwwhvl.mongodb.net/veeras',
     };
 PORT = 4000
