3e3d8b276c8ce62a9d18e60447366d556b3ad2b9
Prakash-Balu <prakash.engg89@gmail.com>
2024-06-13 15:33:13 +0530

    Swagger changes
    Swagger changes

--- swagger.js
+++ swagger.js
@@ -1,11 +1,22 @@
 const swaggerAutogen = require("swagger-autogen")();
 const doc = {
+  openapi: '3.0.0',
   info: {
     title: "Veeras-Education-App",
     description: "Version 2.0"
   },
   host: "veeras-api-new.onrender.com",
   // basePath:"/dev",
+  servers: [
+    {
+      url: "http://localhost:4000/",
+      description: "Local Server"
+    },
+    {
+      url: "https://veeras-api-new.onrender.com",
+      description: "Production Server"
+    }
+  ],
 };
 
 const outputFile = "./swagger-output.json";
